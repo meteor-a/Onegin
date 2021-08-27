38f14e0ef1c435cab4da3deed432d507615fc5f6
meteor-a <61750320+meteor-a@users.noreply.github.com>
2021-08-27 12:31:02 +0300

    Quick sort

--- Onegin/Onegin.cpp
+++ Onegin/Onegin.cpp
@@ -6,31 +6,23 @@ int main() {
 
     InputText(rr, &nLines);
 
-    SortStrings(rr, nLines);
+    QuickSort(rr, 0, nLines - 1);
+
+    // SortStrings(rr, nLines);
 
     OutputSortStrings(rr, nLines);
 
     return OK_RESULT;
 }
 
-int OutputSortStrings(wchar_t** rr, int nLines) {
-    setlocale(LC_CTYPE, "rus");
-
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-
-    printf("Sort by code symbol:\n");
-    for (int ii = 0; ii < nLines; ++ii) {
-        wprintf(L"%i: %s\n", ii + 1, *rr);
-        if (ii + 1 != nLines) {
-            rr = rr + 1;
-        }
-    }
-    
-    return OK_RESULT;
+void swap_str(wchar_t* first, wchar_t* second) {
+    wchar_t tmp[BuffSize] = {};
+    wcscpy(tmp, first);
+    wcscpy(first, second);
+    wcscpy(second, tmp);
 }
 
-int InputText(wchar_t** rr, int *nLines) {
+int InputText(wchar_t** rr, int* nLines) {
     FILE* file_text = nullptr;
     file_text = fopen(Path, "rt");
     if (!file_text) {
@@ -74,16 +66,70 @@ int InputText(wchar_t** rr, int *nLines) {
 }
 
 int SortStrings(wchar_t** rr, int nLines) {
-    wchar_t tmp[BuffSize] = {};
     for (int ii = 0; ii < nLines; ++ii) {
         for (int jj = ii + 1; jj < nLines; ++jj) {
             if (wcscmp(rr[ii], rr[jj]) > 0) {
-                wcscpy(tmp, rr[ii]);
-                wcscpy(rr[ii], rr[jj]);
-                wcscpy(rr[jj], tmp);
+                swap_str(rr[jj], rr[ii]);
             }
         }
     }
 
     return OK_RESULT;
 }
+
+int Partition(wchar_t** a, int low, int high) {
+    int pivot, index, i;
+    index = low;
+    pivot = high;
+
+    for (i = low; i < high; i++) {
+        if (wcscmp(a[i], a[pivot]) < 0) {
+            swap_str(a[i], a[index]);
+            ++index;
+        }
+    }
+    swap_str(a[pivot], a[index]);
+
+    return index;
+}
+
+int RandomPivotPartition(wchar_t** a, int low, int high) {
+    int pvt, n, temp;
+    n = rand();
+    pvt = low + n % (high - low + 1);
+    swap_str(a[high], a[pvt]);
+
+    return Partition(a, low, high);
+}
+
+int QuickSort(wchar_t** a, int low, int high) {
+    int pindex = 0;
+
+    if (low < high) {
+        pindex = RandomPivotPartition(a, low, high);
+        QuickSort(a, low, pindex - 1);
+        QuickSort(a, pindex + 1, high);
+    }
+
+    return 0;
+}
+
+
+int OutputSortStrings(wchar_t** rr, int nLines) {
+    setlocale(LC_CTYPE, "rus");
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    printf("Sort by code symbol:\n");
+    for (int ii = 0; ii < nLines; ++ii) {
+        wprintf(L"%i: %s\n", ii + 1, *rr);
+        if (ii + 1 != nLines) {
+            rr = rr + 1;
+        }
+    }
+
+    return OK_RESULT;
+}
+
+
--- Onegin/Onegin.h
+++ Onegin/Onegin.h
@@ -7,11 +7,15 @@
 
 const int CODE_ERROR = -1;
 const int OK_RESULT = 1;
-const int BuffSize = 200;
-const int SizeText = 9000;
+const int BuffSize = 250;
+const int SizeText = 10000;
 char const* const Path = "text_on.txt";
 
 int OutputSortStrings(wchar_t**, int);
 int InputText(wchar_t**, int*);
 int SortStrings(wchar_t**, int);
+
+int Partition(wchar_t**, int, int);
+int RandomPivotPartition(wchar_t**, int, int);
+int QuickSort(wchar_t**, int, int);
 

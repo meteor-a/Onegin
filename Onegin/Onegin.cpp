bb7192c7a04fd688af7b37ca7dd10afc143886bc
meteor-a <61750320+meteor-a@users.noreply.github.com>
2021-08-27 20:36:37 +0300

    new version

--- Onegin/Onegin.cpp
+++ Onegin/Onegin.cpp
@@ -1,153 +1,80 @@
 ﻿#include "Onegin.h"
 
-int main() {
-    wchar_t* text_str[FILE_SIZE];
-    int nLines = 0;
-    
-    InputText(text_str, &nLines);
-    
-    QuickSort(text_str, 0, nLines - 1);
-
-    //SortStrings(text_str, nLines);
-
-    OutputSortStrings(text_str, nLines);
-    
-    return OK_RESULT;
-}
-
 void swap_str(wchar_t** first, wchar_t** second) {
-    assert(first != nullptr);
-    assert(second != nullptr);
-
     wchar_t* tmp = nullptr;
     tmp = *first;
     *first = *second;
     *second = tmp;
 }
 
-int InputText(wchar_t** text_str, int* nLines) {
-    assert(*text_str != nullptr);
-    assert(nLines != nullptr);
-
-    FILE* file_text = nullptr;
-    file_text = fopen(PATH_FILE_TEXT, "rt");
-    if (!file_text) {
-        printf("Error open text file!\n");
-        return CODE_ERROR;
-    }
-
-    char str[BUFFER_SIZE] = {};
-    wchar_t* res[BUFFER_SIZE];
-
-    while (!feof(file_text)) {
-        *res = (wchar_t*)calloc(BUFFER_SIZE, sizeof(wchar_t));
-
-        fgets(str, BUFFER_SIZE, file_text);
-        ::MultiByteToWideChar(CP_UTF8, 0, str, BUFFER_SIZE, res[0], BUFFER_SIZE);
-
-        for (int ii = 0; ii < wcslen(res[0]); ++ii) {
-            if ((res[0])[ii] == ' ' || (res[0])[ii] == '\n' || (res[0])[ii] == '\t' ||
-                (res[0])[ii] == '\r' || (res[0])[ii] == 160) { // 160 - code of Non-breaking space
-
-                for (int jj = ii; jj < wcslen(res[0]) - 1; ++jj) {
-                    (res[0])[jj] = (res[0])[jj + 1];
-                }
-                (res[0])[wcslen(res[0]) - 1] = '\000';
-
-                --ii;
-            }
-        }
-
-        if (wcslen(res[0]) != 0) {
-            *text_str = (wchar_t*)malloc(sizeof(res[0]));
-            *text_str = res[0];
-            text_str = text_str + 1;
-            ++(*nLines);
-        }
-    }
-
-    fclose(file_text);
-
-    return OK_RESULT;
-}
-
-int SortStrings(wchar_t** text_str, int nLines) {
-    assert(text_str != nullptr);
-
-    for (int ii = 0; ii < nLines; ++ii) {
-        for (int jj = ii + 1; jj < nLines; ++jj) {
-            if (wcscmp(text_str[ii], text_str[jj]) > 0) {
-                swap_str(&text_str[jj], &text_str[ii]);
-            }
-        }
-    }
-
-    return OK_RESULT;
-}
-
-int Partition(wchar_t** text_str, int low, int high) {
-    assert(text_str != nullptr);
-
-    int pivot = 0;
-    int index = 0;
-    index = low;
-    pivot = high;
-
-    for (int ii = low; ii < high; ++ii) {
-        if (wcscmp(text_str[ii], text_str[pivot]) < 0) {
-            swap_str(&text_str[ii], &text_str[index]);
-            ++index;
-        }
-    }
-    swap_str(&text_str[pivot], &text_str[index]);
-
-    return index;
-}
-
-int RandomPivotPartition(wchar_t** text_str, int low, int high) {
-    assert(text_str != nullptr);
-
-    int pvt = 0;
-    int n = 0;
-    int temp = 0;
-    n = rand();
-    pvt = low + n % (high - low + 1);
-    swap_str(&text_str[high], &text_str[pvt]);
-
-    return Partition(text_str, low, high);
-}
-
-int QuickSort(wchar_t** text_str, int low, int high) {
-    assert(text_str != nullptr);
-
-    int pindex = 0;
-
-    if (low < high) {
-        pindex = RandomPivotPartition(text_str, low, high);
-        QuickSort(text_str, low, pindex - 1);
-        QuickSort(text_str, pindex + 1, high);
-    }
-
-    return 0;
-}
-
-int OutputSortStrings(wchar_t** text_str, int nLines) {
-    assert(text_str != nullptr);
-
+int main() {
     setlocale(LC_CTYPE, "rus");
 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    printf("Sort by code symbol:\n");
-    for (int ii = 0; ii < nLines; ++ii) {
-        wprintf(L"%i: %s\n", ii + 1, *text_str);
-        if (ii + 1 != nLines) {
-            text_str = text_str + 1;
+    wchar_t** text_str;
+    int nLines = 0;
+
+    FILE* file_text;
+    file_text = fopen("text_on.txt", "r");
+
+    // Проверка открытия файла
+    if (file_text == NULL) {
+        printf("error\n");
+        return -1;
+    } else {
+        printf("complete\n");
+    }
+
+    fseek(file_text, 0L, SEEK_END);
+    int sz = ftell(file_text);
+    rewind(file_text);
+    text_str = (wchar_t**)calloc(sz, sizeof(wchar_t));
+
+    char str[BUFFER_SIZE] = {};
+    wchar_t* res;
+
+    while (!feof(file_text)) {
+        res = (wchar_t*)calloc(BUFFER_SIZE, sizeof(wchar_t));
+
+        fgets(str, BUFFER_SIZE, file_text);
+        ::MultiByteToWideChar(CP_UTF8, 0, str, BUFFER_SIZE, res, BUFFER_SIZE);
+
+        for (int ii = 0; ii < wcslen(res); ++ii) {
+            if (*(res + ii) == '\n' || *(res + ii) == '\r' || *(res + ii) == '\t' ||
+                *(res + ii) == 160 || *(res + ii) == ' ') {
+
+                for (int jj = ii; jj < wcslen(res) - 1; ++jj) {
+                    *(res + jj) = *(res + jj + 1);
+                }
+                *(res + wcslen(res) - 1) = '\000';
+
+                --ii;
+            }
+        }
+
+        if (wcslen(res) != 0) {
+            *text_str = (wchar_t*)malloc(sizeof(res));
+            *(text_str + nLines) = res;
+            ++nLines;
         }
     }
 
-    return OK_RESULT;
+    fclose(file_text);
+
+    for (int ii = 0; ii < nLines; ++ii) {
+        for (int jj = ii + 1; jj < nLines; ++jj) {
+            if (wcscmp(*(text_str + ii), *(text_str + jj)) > 0) {
+                swap_str((text_str + ii), (text_str + jj));
+            }
+        }
+    }
+
+    printf("Sort by code symbol:\n");
+    for (int ii = 0; ii < nLines; ++ii) {
+        wprintf(L"%i: %s\n", ii + 1, *(text_str + ii));
+    }
+
+    return 1;
 }
-
-
--- Onegin/Onegin.h
+++ Onegin/Onegin.h
@@ -1,22 +1,9 @@
 #pragma once
+#include < stdio.h >  //Äëÿ printf, fgets, fopen, fclose, feof
 #include <stdio.h>
 #include <stdlib.h>
-#include <wchar.h>
-#include <assert.h>
 #include <Windows.h>
+#include <assert.h>
 #include <locale.h>
 
-const int CODE_ERROR = -1;
-const int OK_RESULT = 1;
-const int BUFFER_SIZE = 3000;
-const int FILE_SIZE = 10000;
-char const* const PATH_FILE_TEXT = "text_on.txt";
-
-int OutputSortStrings(wchar_t**, int);
-int InputText(wchar_t**, int*);
-int SortStrings(wchar_t**, int);
-
-int Partition(wchar_t**, int, int);
-int RandomPivotPartition(wchar_t**, int, int);
-int QuickSort(wchar_t**, int, int);
-
+const int BUFFER_SIZE = 1500;

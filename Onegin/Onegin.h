30a3a72cdb6c88e204268566d26e41fc4b9080fa
meteor-a <61750320+meteor-a@users.noreply.github.com>
2021-09-07 23:55:42 +0300

    13

--- Onegin/Onegin.h
+++ Onegin/Onegin.h
@@ -1,28 +1,28 @@
 #pragma once
-#include < stdio.h >  //Äëÿ printf, fgets, fopen, fclose, feof
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <wchar.h> 
+#include <malloc.h>
 #include <Windows.h>
 #include <assert.h>
 #include <locale.h>
-#include <algorithm>
 
-const int BUFFER_SIZE = 1500;
 const int OK_RESULT = 1;
 const int CODE_ERROR = -9;
-const char PATH_FILE[] = { "text_on.txt" };
 
-struct StringStruct {
-    wchar_t str[BUFFER_SIZE];
-};
+const char PATH_FILE_ONEGIN[] = { "text_on.txt" };
+const char PATH_FILE_OUTPUT[] = { "output.txt" };
 
-void swap_str(wchar_t**, wchar_t**);
+int RandGenStr();
 
-int OutputSortStrings(wchar_t**, int);
-int SortStrings(wchar_t**, int);
-wchar_t** InputStrings(wchar_t**, int*);
+wchar_t** InputText(long long*, int*);
+void OutputConsole(wchar_t**, long long);
+void OutputFile(wchar_t**, long long);
 
 int getFileSize(FILE*);
+FILE* OpenFile(const char*, const char*);
+int CloseFile(FILE*);
 
+int comparator(const void*, const void*);
 

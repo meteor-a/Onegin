a0915139f90278c6014e65043ec5713fe655aa76
meteor-a <61750320+meteor-a@users.noreply.github.com>
2021-09-17 21:09:00 +0300

    cmd

--- Onegin/Onegin.cpp
+++ Onegin/Onegin.cpp
@@ -2,10 +2,16 @@
 
 int main(int argc, char* argv[]) {
     SetOutputSettings();
-
-    printf("Input name of text file: ");
     char file_name[100];
-    scanf("%s", file_name);
-    int result = SortGenText(file_name);
+    int result = 0;
+    if (argc > 1) {
+        result = SortGenText(argv[1]);
+    }
+    else {
+        printf("Input name of text file: ");
+        scanf("%s", file_name);
+        result = SortGenText(file_name);
+    }
+
     return result;
 }
